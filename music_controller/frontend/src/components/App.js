222ede9b1745303d15fbabc6689c1eabe63739cb
Yontwises <kenny.yontwises@gmail.com>
2022-07-13 04:07:36 -0700

    Added Routers

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import Homepage from './homepage';
+
 
 export default class App extends Component {
     constructor(props) {
@@ -8,9 +10,13 @@ export default class App extends Component {
 
 
     render() {
-        return <h1>{this.props.name}</h1>;
+        return (
+            <div>
+                <Homepage />
+            </div>
+        );
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App name="tim" />, appDiv);
+render(<App />, appDiv);
---
+++ music_controller/frontend/src/components/homepage.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import RoomJoinPage from "./RoomJoinPage";
+import CreateRoomPage from "./CreateRoomPage";
+import { BrowserRouter as Router, Routes, Route, Link, Redirect } from "react-router-dom"
+
+export default class Homepage extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Router>
+                <Routes>
+                    <Route path='/' element={<p>This is the homepage</p>} />
+                    <Route path='/join' element={<RoomJoinPage/> } />
+                    <Route path='/create' element={<CreateRoomPage/>} />
+                </Routes>
+
+            </Router>
+        );
+    }
+}
